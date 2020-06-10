ee1de59d375bf07b6b1b7c35fd7f7e137d5523ce
hoangtruongbao <baohoang147789@gmail.com>
2020-06-10 10:05:43 +0700

    first commit

---
+++ build/precache-manifest.9260ebdc594dc745bcc4adcdbed6944a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50712c1a6762390cb1b5db69e93dc774",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9e01a9326eed7cde889",
+    "url": "/static/css/main.31c75035.chunk.css"
+  },
+  {
+    "revision": "5945a1d2908187eef503",
+    "url": "/static/js/2.fb14ba2b.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.fb14ba2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9e01a9326eed7cde889",
+    "url": "/static/js/main.6996ae15.chunk.js"
+  },
+  {
+    "revision": "b176a8271c314386e346",
+    "url": "/static/js/runtime-main.9108faa3.js"
+  }
+]);
